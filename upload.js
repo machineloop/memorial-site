5e21a797d8a3bf2726ef63ab5422a325274a9804
machineloop <3682072+machineloop@users.noreply.github.com>
2023-12-13 17:56:25 +0000

    Change to submit input instead of button

--- upload.js
+++ upload.js
@@ -28,7 +28,7 @@ function handleFileUploadChange(e) {
 
 function handleFileUploadSubmit(e) {
     // Upload file and metadata to the object 'images/mountains.jpg'
-    submitterName = document.querySelector('#name_submitter').nodeValue;
+    submitterName = document.querySelector('#name_submitter').value || '';
     fileType = selectedFile.type || '';
     metadata = {
         contentType: `${fileType}`,
